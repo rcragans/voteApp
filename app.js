7d84c1c9eba303c8bcd31d3a785a1cae6cf41285
Robert Ragans <robertragans@Roberts-MacBook-Pro-2.local>
2019-01-28 15:21:37 -0500

    db layer added

--- app.js
+++ app.js
@@ -15,9 +15,24 @@ app.set('view engine','ejs')
 app.use(express.static('public'))
 
 app.get('/',(req,res,next)=>{
-    res.render('index',{})
+    const animalQuery = `SELECT * FROM animals;`
+    connection.query(animalQuery,(error,results)=>{
+        const rand = Math.floor(Math.random() * results.length)
+        res.render('index',{animal:results[rand]})
+    })
+    
 })
 
+app.get('/vote/:value/:id',(req,res,next)=>{
+    const value = req.params.value 
+    const id = req.params.id
+    const insertQuery = `INSERT INTO votes (id,aid,value)
+    VALUES (DEFAULT,?,?)`
+    connection.query(insertQuery,[id,value],(error,results)=>{
+        if (error){throw error}
+        res.redirect('/')
+    })
+})
 console.log("App is listening on port 8282")
 
 app.listen(8282)
